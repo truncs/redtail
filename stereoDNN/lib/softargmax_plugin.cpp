c57dd0d634ba4e447e3dd365f7b6a54e53180423
mtbsteve <schiwo1@gmail.com>
2019-10-16 16:31:19 +0200

    Revert "Dev/vijay609/xavier"

--- stereoDNN/lib/softargmax_plugin.cpp
+++ stereoDNN/lib/softargmax_plugin.cpp
@@ -46,6 +46,8 @@ public:
         createDescriptors();
     }
 
+    SoftargmaxPlugin(SoftargmaxPlugin&&) = delete;
+
     bool supportsFormat(DataType type, PluginFormat format) const override
     {
         return (type == data_type_) && (format == PluginFormat::kNCHW);
@@ -155,28 +157,6 @@ public:
 
         assert(!isValid());
     }
-
-#ifdef TENSORRT_5
-    const char* getPluginType() const override
-    {
-        return name_.c_str();
-    }
-
-    const char* getPluginVersion() const override
-    {
-        return "default";
-    }
-
-    void destroy() override
-    {
-        delete this;
-    }
-
-    virtual IPluginExt* clone() const override
-    {
-        return static_cast<IPluginExt*>(new SoftargmaxPlugin(*this));
-    }
-#endif
 
     size_t getWorkspaceSize(int maxBatchSize) const
     {
